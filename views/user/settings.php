6f78a27b2fd54ec08866531318cb46b4bee4ea63
Zorkpt <hugopocas@gmail.com>
2023-07-07 10:26:23 +0100

    Fixed bug in user settings

--- views/user/settings.php
+++ views/user/settings.php
@@ -2,7 +2,7 @@
 <?php require BASE_PATH . "/views/partials/nav.php"; ?>
 <?php $header = 'Perfil'; ?>
 <?php require BASE_PATH . "/views/partials/banner.php"; ?>
-
+<div class="mb-auto">
 
 <main class="flex-grow">
     <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
