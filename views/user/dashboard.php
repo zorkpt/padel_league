1bdbb90329887dd1e8f2eeff675f2afaf3998847
Zorkpt <hugopocas@gmail.com>
2023-07-07 13:38:16 +0100

    Added "Inserir Codigo de Convite" to the dashboard view

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -41,6 +41,11 @@ extract($leagues);
                class="mx-auto unsubscribe-button rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600">
                 Criar nova Liga</a>
         </div>
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <a href="/league/join"
+               class="mx-auto unsubscribe-button rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-600">
+                Inserir Código Convite</a>
+        </div>
     </div>
 
 </main>
