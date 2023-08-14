c4aa0619d3c70cc5128f7b4abf13a792dc5a1312
Zorkpt <hugopocas@gmail.com>
2023-08-14 01:45:46 +0100

    minor bug fix

--- views/user/dashboard.php
+++ views/user/dashboard.php
@@ -5,6 +5,8 @@
 extract($leagues);
 extract($lastGames);
 ?>
+<div class="mb-auto">
+
 <header class="bg-white shadow">
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <div class="flex flex-col md:flex-row justify-between items-center">
