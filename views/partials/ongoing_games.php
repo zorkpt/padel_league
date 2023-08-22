21e972ac326365767bbc1b9fc54c3c482318bb70
Hugo Poças <hugopocas@MacBook-Pro-de-Hugo.local>
2023-08-22 14:51:28 +0100

    Registering results only after balanced teams

--- views/partials/ongoing_games.php
+++ views/partials/ongoing_games.php
@@ -21,12 +21,12 @@
             <tbody class="bg-white divide-y divide-gray-200">
             <?php foreach ($ongoingLeagueGames as $game) { ?>
                 <tr>
-                    <td class="px-2 py-4 whitespace-nowrap text-sm text-gray-500"><?= htmlspecialchars((new DateTime($game['data_hora']))->format("d/m")) ?></td>
-                    <td class="px-2 py-4 whitespace-nowrap text-sm text-gray-500"><?= htmlspecialchars((new DateTime($game['data_hora']))->format("H:i")) ?></td>
-                    <td class="px-6 py-4 items-center"><?= htmlspecialchars($game['local']) ?></td>
+                    <td class="px-2 py-4 whitespace-nowrap text-center text-sm text-gray-500"><?= htmlspecialchars((new DateTime($game['data_hora']))->format("d/m")) ?></td>
+                    <td class="px-2 py-4 whitespace-nowrap text-center text-sm text-gray-500"><?= htmlspecialchars((new DateTime($game['data_hora']))->format("H:i")) ?></td>
+                    <td class="px-6 py-4 text-center items-center"><?= htmlspecialchars($game['local']) ?></td>
 
                     <td class="px-6 py-4 items-center">
-                        <div class="justify-center">
+                        <div class="text-center justify-center">
                             <a class="inline-flex items-center justify-center px-4 py-2 text-xs font-medium text-white uppercase transition bg-indigo-600 rounded shadow ripple hover:shadow-lg hover:bg-indigo-800 focus:outline-none min-w-[60px] min-h-[30px]"
                                href="/game?id=<?= htmlspecialchars($game['id']) ?>">Abrir</a>
                         </div>
